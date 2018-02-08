a98c93fa36d327cdcd98e3b596efddea59df91f3
FranciscoBarrero <fbarrero06@gmail.com>
2018-02-08 07:13:07 -0500

    inicio del proyecto

---
+++ src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre;
+        Scanner sc= new Scanner(System.in);
+        nombre=sc.next();
+        System.out.println("hola "+ nombre);
+        
+    }
+    
+}
